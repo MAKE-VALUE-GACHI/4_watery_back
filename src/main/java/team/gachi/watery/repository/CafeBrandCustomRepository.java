5d5b8bcaf0caf7dc617a68182fef52b2177258e6
hobit22 <hobit22@naver.com>
2025-06-22 15:37:23 +0900

    Chore: 불필요 파일 삭제

--- src/main/java/team/gachi/watery/repository/CafeBrandCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package team.gachi.watery.repository;
-
-import team.gachi.watery.domain.CafeBrand;
-
-import java.util.List;
-
-public interface CafeBrandCustomRepository {
-    List<CafeBrand> findAllByParam(String name);
-}
--- src/main/java/team/gachi/watery/repository/CafeBrandRepository.java
+++
@@ -1,7 +0,0 @@
-package team.gachi.watery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team.gachi.watery.domain.CafeBrand;
-
-public interface CafeBrandRepository extends JpaRepository<CafeBrand, Long>, CafeBrandCustomRepository {
-}

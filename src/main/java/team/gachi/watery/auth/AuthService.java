20ae60315b4d27f4c198f9eb5a5fffcf9cd9d853
hobit22 <hobit22@naver.com>
2025-06-27 23:30:57 +0900

    feat: authService 추가

---
+++ src/main/java/team/gachi/watery/auth/AuthService.java
@@ -0,0 +1,36 @@
+package team.gachi.watery.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.gachi.watery.auth.dto.SignInRequest;
+import team.gachi.watery.auth.dto.SignInResponse;
+import team.gachi.watery.domain.User;
+import team.gachi.watery.oauth.OAuthService;
+import team.gachi.watery.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+    private final TokenGenerator tokenGenerator;
+    private final UserRepository userRepository;
+    private final OAuthService oAuthService;
+
+    @Transactional
+    public SignInResponse signIn(String socialAccessToken, SignInRequest request) {
+        User.Social social = oAuthService.login(request.socialType(), socialAccessToken);
+        User signedUser = signIn(social, request);
+        signedUser.updateTokenInLogin(
+                tokenGenerator.generateRefreshToken(signedUser.getId()),
+                request.fcmToken());
+
+        return SignInResponse.of(tokenGenerator.generateAccessToken(signedUser.getId()), signedUser);
+    }
+
+    private User signIn(User.Social social, SignInRequest request) {
+        return userRepository.findBySocialTypeAndSocialId(social.socialType(), social.socialId())
+                .orElseGet(() -> userRepository.save(request.toDomain(social.socialId())));
+    }
+}

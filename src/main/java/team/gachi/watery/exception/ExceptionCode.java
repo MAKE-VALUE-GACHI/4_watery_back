c8172369f32a46b042d4b1ea5ef01369e4c605a8
hobit22 <hobit22@naver.com>
2025-07-03 01:39:46 +0900

    Chore: exception message 수정

--- src/main/java/team/gachi/watery/exception/ExceptionCode.java
+++ src/main/java/team/gachi/watery/exception/ExceptionCode.java
@@ -7,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ExceptionCode {
     // 4xx
-    UNAUTHORIZED(401, "유효하지 않은 토큰 "),
-    NOT_FOUND(404, "존재하지 않음 "),
+    UNAUTHORIZED(401, "유효하지 않은 토큰"),
+    NOT_FOUND(404, "존재하지 않음"),
     INVALID_ENUM_TYPE(400, "유효하지 않은 Enum 값"),
 
     // 5xx
-    SERVICE_AVAILABLE(503, "서비스에 접근할 수 없음 "),
+    SERVICE_AVAILABLE(503, "서비스에 접근할 수 없음"),
     INTERNAL_SERVER_ERROR(500, "서버 내부 오류"),
     JSON_PARSE_ERROR(503, "json 파싱 오류. 서버에게 문의해주세요."),
     ;

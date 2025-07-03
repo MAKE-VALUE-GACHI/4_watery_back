13ca792938ddc24671f745a119e8da5c555d30f0
hobit22 <hobit22@naver.com>
2025-07-03 19:55:30 +0900

    Feat: auth sign-in swagger 수정

--- src/main/java/team/gachi/watery/auth/AuthController.java
+++ src/main/java/team/gachi/watery/auth/AuthController.java
@@ -1,6 +1,8 @@
 package team.gachi.watery.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import team.gachi.watery.auth.dto.SignInRequest;
@@ -13,9 +15,13 @@ import team.gachi.watery.dto.WateryResponse;
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(
+            summary = "소셜 로그인",
+            description = "소셜서비스 Access Token을 Authorization 헤더에 담아 로그인합니다."
+    )
     @PostMapping("/sign-in")
     public WateryResponse<SignInResponse> signIn(
-            @Parameter(description = "소셜 로그인 액세스 토큰", example = "Bearer kakao-access-token")
+            @Parameter(hidden = true)
             @RequestHeader("Authorization") String socialAccessToken,
             @RequestBody SignInRequest request
     ) {

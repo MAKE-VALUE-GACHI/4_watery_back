d7378bbf649bba7fcb781a2b57874654e7c97e5a
hobit22 <hobit22@naver.com>
2025-07-03 19:34:03 +0900

    Feat: timezone, security 설정

--- src/main/java/team/gachi/watery/WateryApplication.java
+++ src/main/java/team/gachi/watery/WateryApplication.java
@@ -3,10 +3,13 @@ package team.gachi.watery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class WateryApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(WateryApplication.class, args);
     }
 

85b76e612cf854f8c28ec522211163d1ba9c0033
hobit22 <hobit22@naver.com>
2025-07-13 17:35:25 +0900

    Feat: executor 설정 수정

--- src/main/java/team/gachi/watery/config/AsyncConfig.java
+++ src/main/java/team/gachi/watery/config/AsyncConfig.java
@@ -16,9 +16,9 @@ public class AsyncConfig implements AsyncConfigurer {
     public Executor asyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 
-        executor.setCorePoolSize(4); // 최소 스레드 수
-        executor.setMaxPoolSize(10); // 최대 스레드 수
-        executor.setQueueCapacity(100); // 대기 큐 용량
+        executor.setCorePoolSize(1); // 최소 스레드 수
+        executor.setMaxPoolSize(2); // 최대 스레드 수
+        executor.setQueueCapacity(10); // 대기 큐 용량
         executor.setThreadNamePrefix("AsyncExecutor-");
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.setAwaitTerminationSeconds(10);

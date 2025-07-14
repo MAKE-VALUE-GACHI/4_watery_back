01771ac9c6f90898cfe746a062ecc0e5ccb4e80b
junhyeong00 <jhbae0420@naver.com>
2025-07-15 06:34:40 +0900

    Chore: Drink 충돌 해결

--- src/main/java/team/gachi/watery/drink/domain/Drink.java
+++ src/main/java/team/gachi/watery/drink/domain/Drink.java
@@ -11,8 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
